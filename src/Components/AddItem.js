296d2ac90261887040131b7ac5c53f7baf576d85
Rebecca Botha <rb50@users.noreply.github.com>
2021-01-04 20:53:15 +0000

    create list and additem views including bold links when clicked

---
+++ src/Components/AddItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class AddItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>AddItem</h1>;
+  }
+}
+
+export default AddItem;
---
+++ src/Components/List.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>List</h1>;
+  }
+}
+
+export default List;
