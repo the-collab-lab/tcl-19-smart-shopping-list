b81776a22dc05b214614a6885af6cb9ed5ceaaae
Amaka <amakaumeh01@gmail.com>
2021-01-09 08:33:58 +0100

    rename component directory with lowercase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import List from './Components/List';
-import AddItem from './Components/AddItem';
-import Nav from './Components/Nav';
+import List from './components/List';
+import AddItem from './components/AddItem';
+import Nav from './components/Nav';
 
 function App() {
   return (
