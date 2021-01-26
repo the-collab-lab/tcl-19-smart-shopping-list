e30e9eed16218d31a2ebcccfe510071d03eb41e7
nibble0101 <mj.mawa@yahoo.com>
2021-01-26 20:43:05 +0300

    Added code to check if an item was purchased within the last 24 hours

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -5,6 +5,11 @@ import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 const db = firebase.firestore().collection('shopping_list');
 
+const wasItemPurchasedWithinLastOneDay = (lastPurchasedOn) => {
+  const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
+  return Date.now() - lastPurchasedOn <= oneDayInMilliseconds;
+};
+
 const ItemsList = () => {
   const userToken = localStorage.getItem('token');
 
@@ -12,17 +17,30 @@ const ItemsList = () => {
     db.where('token', '==', userToken),
     { idField: 'documentId' },
   );
+  /*
+   This uses the client's local time. If there is a time zone difference,
+   it is likely to cause issues because a user can invite another person
+   to join the shopping list.
+  */
   const markItemAsPurchased = (index) => {
     const { items, documentId } = shoppingList[0];
     const shoppingListObject = items[index];
-    if (shoppingListObject.lastPurchasedOn !== null) {
-      return;
+
+    if (shoppingListObject.lastPurchasedOn === null) {
+      shoppingListObject.lastPurchasedOn = Date.now();
+    } else {
+      shoppingListObject.lastPurchasedOn = null;
     }
-    shoppingListObject.lastPurchasedOn = Date.now();
+
     items[index] = shoppingListObject;
     db.doc(documentId)
       .update({
         items: items,
+        /*
+        Research whether it is possible to update a single item within items array
+        by inserting it at a particular index instead of updating entire array of 
+        items
+        */
       })
       .then(() => console.log('Successfully updated item'))
       .catch((e) => console.log('error', e));
@@ -50,7 +68,9 @@ const ItemsList = () => {
                       checked={
                         shoppingItemObject.lastPurchasedOn === null
                           ? false
-                          : true
+                          : wasItemPurchasedWithinLastOneDay(
+                              shoppingItemObject.lastPurchasedOn,
+                            )
                       }
                     />
                   </label>
