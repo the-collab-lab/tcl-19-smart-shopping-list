35cc90f605acf049d88dbdb3d75dc816f7e593e3
Rebecca Botha <rb50@users.noreply.github.com>
2021-02-02 15:06:32 +0000

    implement filter of list

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -15,6 +15,11 @@ const wasItemPurchasedWithinLastOneDay = (lastPurchasedOn) => {
 };
 
 const ItemsList = () => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   const userToken = localStorage.getItem('token');
   const history = useHistory();
 
@@ -22,7 +27,6 @@ const ItemsList = () => {
     db.where('token', '==', userToken),
     { idField: 'documentId' },
   );
-
 
   const markItemAsPurchased = (index) => {
     const { items, documentId } = shoppingList[0];
@@ -43,41 +47,57 @@ const ItemsList = () => {
     history.push('/additem');
   };
 
-
   return (
     <div className="items-list">
       <h1>Your Shopping List</h1>
       {loading && <p>Loading...</p>}
       {error && <p>An error has occured...</p>}
       {shoppingList && !shoppingList.length && (
-            <div className="add-item">
-              <p>You haven't created a shopping list yet...</p>
-              <button type="submit" onClick={handleRedirect}>
-                Add First Item
-              </button>
-            </div>
-          )}
+        <div className="add-item">
+          <p>You haven't created a shopping list yet...</p>
+          <button type="submit" onClick={handleRedirect}>
+            Add First Item
+          </button>
+        </div>
+      )}
       <form>
+        {shoppingList && shoppingList[0] ? (
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchTerm}
+            onChange={handleChange}
+          />
+        ) : null}
         <ul>
           {shoppingList &&
             shoppingList[0] &&
-            shoppingList[0].items.map((shoppingItemObject, index) => {
-              return (
-                <li key={shoppingItemObject.shoppingListItemName + index}>
-                  <input
-                    type="checkbox"
-                    id={shoppingItemObject.shoppingListItemName}
-                    onChange={() => markItemAsPurchased(index)}
-                    checked={wasItemPurchasedWithinLastOneDay(
-                      shoppingItemObject.lastPurchasedOn,
-                    )}
-                  />
-                  <label htmlFor={shoppingItemObject.shoppingListItemName}>
-                    {shoppingItemObject.shoppingListItemName}
-                  </label>
-                </li>
-              );
-            })}
+            shoppingList[0].items
+              .filter((shoppingItemObject) =>
+                shoppingItemObject.shoppingListItemName
+                  .toLowerCase()
+                  .includes(searchTerm.toLowerCase()),
+              )
+              .map((shoppingItemObject, index) => {
+                const shopIndex = shoppingList[0].items.indexOf(
+                  shoppingItemObject,
+                );
+                return (
+                  <li key={shoppingItemObject.shoppingListItemName + index}>
+                    <input
+                      type="checkbox"
+                      id={shoppingItemObject.shoppingListItemName}
+                      onChange={() => markItemAsPurchased(shopIndex)}
+                      checked={wasItemPurchasedWithinLastOneDay(
+                        shoppingItemObject.lastPurchasedOn,
+                      )}
+                    />
+                    <label htmlFor={shoppingItemObject.shoppingListItemName}>
+                      {shoppingItemObject.shoppingListItemName}
+                    </label>
+                  </li>
+                );
+              })}
         </ul>
       </form>
       <Nav />
