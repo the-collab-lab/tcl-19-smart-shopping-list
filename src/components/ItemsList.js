25c5819dbba3a34b70f63d700d6e4d4e1eee999e
nibble0101 <mj.mawa@yahoo.com>
2021-02-02 11:59:58 +0300

    added method 1

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -4,6 +4,7 @@ import Nav from './Nav';
 import '../styles/ItemsList.css';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { useHistory } from 'react-router-dom';
+import calculateEstimate from '../lib/estimates';
 import '../styles/ItemsList.css';
 
 const db = firebase.firestore().collection('shopping_list');
@@ -12,6 +13,14 @@ const wasItemPurchasedWithinLastOneDay = (lastPurchasedOn) => {
   if (lastPurchasedOn === null) return false;
   const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
   return Date.now() - lastPurchasedOn <= oneDayInMilliseconds;
+};
+
+const getDaysBetweenCurrentAndPreviousPurchase = (
+  previousPurchaseDate,
+  currentPurchaseDate = Date.now(),
+) => {
+  const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
+  return (currentPurchaseDate - previousPurchaseDate) / oneDayInMilliseconds;
 };
 
 const ItemsList = () => {
@@ -23,13 +32,53 @@ const ItemsList = () => {
     { idField: 'documentId' },
   );
 
-
-  const markItemAsPurchased = (index) => {
+  const markItemAsPurchased = (index, event) => {
     const { items, documentId } = shoppingList[0];
-
-    items[index].lastPurchasedOn = items[index].lastPurchasedOn
-      ? null
-      : Date.now();
+    const { lastPurchasedOn } = items[index];
+    const shoppingItemObject = items[index];
+    if (lastPurchasedOn === null) {
+      shoppingItemObject.lastPurchasedOn = Date.now();
+      shoppingItemObject.numberOfPurchases++;
+      shoppingItemObject.daysLeftForNextPurchase = calculateEstimate(
+        undefined,
+        shoppingItemObject.daysLeftForNextPurchase,
+        shoppingItemObject.numberOfPurchases,
+      );
+    } else {
+      if (wasItemPurchasedWithinLastOneDay(lastPurchasedOn)) {
+        /*
+        This code is evaluated if a user marks an item as purchased and 
+        the user wants to reverse that decision. It is only possible to revert
+        if a user has only recorded their first purchase. In this case, we
+        revert the value of lastPurchasedOn to null, decrement 
+        numberOfPurchases by 1 and maintain the value of daysLeftForNextPurchase,
+        
+        It is not possible to revert if purchase count exceeds 1 because once we 
+        make subsequent purchases the value of lastPurchasedOn is overwritten therefore
+        it is not possible to revert to the previous purchase in the DB.
+        */
+        if (shoppingItemObject.numberOfPurchases === 1) {
+          shoppingItemObject.numberOfPurchases--;
+          shoppingItemObject.lastPurchasedOn = null;
+        } else {
+          // If we are dealing with subsequent purchases we have already lost the
+          // lastPurchasedOn value for the previous purchase, there is no value to
+          // revert to. It doesn't make sense to revert to null either because a
+          // previous purchase has been made.
+          return;
+        }
+      } else {
+        shoppingItemObject.numberOfPurchases++;
+        const { daysLeftForNextPurchase } = shoppingItemObject;
+        const now = Date.now();
+        shoppingItemObject.daysLeftForNextPurchase = calculateEstimate(
+          daysLeftForNextPurchase,
+          getDaysBetweenCurrentAndPreviousPurchase(lastPurchasedOn, now),
+          shoppingItemObject.numberOfPurchases,
+        );
+        shoppingItemObject.lastPurchasedOn = now;
+      }
+    }
 
     db.doc(documentId)
       .update({
@@ -43,20 +92,19 @@ const ItemsList = () => {
     history.push('/additem');
   };
 
-
   return (
     <div className="items-list">
       <h1>Your Shopping List</h1>
       {loading && <p>Loading...</p>}
       {error && <p>An error has occured...</p>}
       {shoppingList && !shoppingList.length && (
-            <div className="add-item">
-              <p>You haven't created a shopping list yet...</p>
-              <button type="submit" onClick={handleRedirect}>
-                Add First Item
-              </button>
-            </div>
-          )}
+        <div className="add-item">
+          <p>You haven't created a shopping list yet...</p>
+          <button type="submit" onClick={handleRedirect}>
+            Add First Item
+          </button>
+        </div>
+      )}
       <form>
         <ul>
           {shoppingList &&
