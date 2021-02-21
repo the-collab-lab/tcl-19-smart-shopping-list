856fee03dfa1d7b870f7dd38603af2bde5758d60
YuriDevAT <JUndeutsch@gmx.at>
2021-02-21 09:22:34 +0100

    changed background colors to mono colors

--- src/components/sortingFunctions.js
+++ src/components/sortingFunctions.js
@@ -10,13 +10,13 @@ export const getShoppingItemBackgroundStyles = (
   );
 
   if (daysBetweenCurrentAndPreviousPurchase >= daysLeftForNextPurchase * 2) {
-    return '#9CA3AF';
+    return '#D1D5DB';
   } else if (daysLeftForNextPurchase <= 7) {
-    return '#EF4444';
+    return '#047857';
   } else if (daysLeftForNextPurchase < 30) {
-    return '#FBBF24';
+    return '#10B981';
   } else {
-    return '#FCA5A5';
+    return '#6EE7B7';
   }
 };
 
@@ -30,8 +30,6 @@ export const getItemDescription = (days) => {
   }
 };
 
-// Compare two elements a and b. If a is bigger than b sort b before a.
-// If a and b are the same sort alphabetically.
 export const sortShoppingList = (a, b) => {
   return (
     a.daysLeftForNextPurchase - b.daysLeftForNextPurchase ||
