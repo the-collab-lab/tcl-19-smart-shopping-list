e3a5bbdb525e9f0e914f9d00449d887f5e7500b0
YuriDevAT <JUndeutsch@gmx.at>
2021-02-22 13:23:44 +0100

    fixed netlify bug

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { ReactComponent as ListButton } from '../img/list-button.svg';
-import { ReactComponent as AddItemButton } from '../img/add-item-button.svg';
+import { ReactComponent as TaskIcon } from '../img/tasks-solid.svg';
+import { ReactComponent as AddIcon } from '../img/plus-solid.svg';
 
 const Nav = () => {
   return (
@@ -9,14 +9,18 @@ const Nav = () => {
       <div className="md:hidden">
         <nav className="h-auto fixed bottom-0 right-0 left-0">
           <ul className="flex justify-center content-center fixed bottom-0 right-0 left-0 m-auto bg-green-300 rounded-t-3xl shadow-top">
-            <li className="list-none mr-8">
+            <li className="list-none mr-8 py-2">
               <NavLink to="/list" activeClassName="font-bold">
-                <ListButton />
+                <button class="rounded-full h-20 w-20 flex items-center justify-center bg-green-600 text-white shadow-xl">
+                  <TaskIcon />
+                </button>
               </NavLink>
             </li>
-            <li className="list-none ml-8">
+            <li className="list-none ml-8 py-2">
               <NavLink to="/addItem" activeClassName="font-bold">
-                <AddItemButton />
+                <button class="rounded-full h-20 w-20 flex items-center justify-center bg-green-600 text-white shadow-xl">
+                  <AddIcon />
+                </button>
               </NavLink>
             </li>
           </ul>
