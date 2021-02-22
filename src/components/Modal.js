5bbbf8ef8811f92d971b05e20971b9c4d7a3bb19
Rebecca Botha <rb50@users.noreply.github.com>
2021-02-22 14:57:11 +0000

    remove commented code

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,13 +2,6 @@ import React, { useState } from 'react';
 import '../styles/Modal.css';
 
 const Modal = (props) => {
-  // const [hidden, setHidden] = useState(props.active);
-
-  // const removeModal = () => {
-  //   console.log('running');
-  //   setHidden(false);
-  // };
-
   return (
     <div className={!props.active ? 'hidden' : null}>
       <div className="style-modal bg-green-500">
