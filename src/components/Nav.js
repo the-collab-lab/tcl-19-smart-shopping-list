66a4ef87daf04cb5fecdd0bd8ea69cb0e40449af
YuriDevAT <JUndeutsch@gmx.at>
2021-02-22 21:05:04 +0100

    fixed netlify bug by using buttons instead of svgs

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { ReactComponent as ListButton } from '../img/list-button.svg';
-import { ReactComponent as AddItemButton } from '../img/add-item-button.svg';
+import { ReactComponent as TaskIcon } from '../img/tasks-solid.svg';
+import { ReactComponent as AddIcon } from '../img/plus-solid.svg';
 
 const Nav = () => {
   return (
-    <div>
-      <div className="md:hidden">
-        <nav className="h-auto fixed bottom-0 right-0 left-0">
-          <ul className="flex justify-center content-center fixed bottom-0 right-0 left-0 m-auto bg-green-300 rounded-t-3xl shadow-top">
-            <li className="list-none mr-8">
-              <NavLink to="/list" activeClassName="font-bold">
-                <ListButton />
-              </NavLink>
-            </li>
-            <li className="list-none ml-8">
-              <NavLink to="/addItem" activeClassName="font-bold">
-                <AddItemButton />
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <div className="md:hidden">
+      <nav className="h-auto fixed bottom-0 right-0 left-0">
+        <ul className="flex justify-center content-center fixed bottom-0 right-0 left-0 bg-green-500 rounded-t-3xl shadow-top">
+          <li className="list-none mr-8 py-2">
+            <NavLink to="/list" a>
+              <button
+                title="Go to list view"
+                className="rounded-full h-20 w-20 flex items-center justify-center bg-green-700 text-gray-100 shadow-xl focus:ring-4 focus:ring-green-200 focus:ring-opacity-50 focus:outline-none"
+              >
+                <TaskIcon />
+              </button>
+            </NavLink>
+          </li>
+          <li className="list-none ml-8 py-2">
+            <NavLink to="/addItem">
+              <button
+                title="Go to add items view"
+                className="rounded-full h-20 w-20 flex items-center justify-center bg-green-700 text-gray-100 shadow-xl focus:ring-4 focus:ring-green-200 focus:ring-opacity-50 focus:outline-none"
+              >
+                <AddIcon />
+              </button>
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
