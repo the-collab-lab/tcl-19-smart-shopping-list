8475b18ee1f3347f5403f425175d8aae661999b8
Rebecca Botha <rb50@users.noreply.github.com>
2021-02-23 10:31:44 +0000

    make elements in the additem view semantic

--- src/components/AddItems.js
+++ src/components/AddItems.js
@@ -94,97 +94,92 @@ const AddItemsToList = () => {
 
   return (
     <div>
-      <div className="max-h-screen flex flex-col box-border items-center">
-        <header className="bg-green-700 w-full fixed text-center">
+      <header className="max-h-screen flex flex-col box-border items-center">
+        <section className="bg-green-700 w-full fixed text-center">
           <h2 className="pt-6 pb-16 text-4xl font-thin text-gray-100">
             Add Item to List
           </h2>
           <span className="text-white top-0 right-0 absolute sm:mt-4 sm:mr-4">
             <HomeIcon />
           </span>
-        </header>
-      </div>
-      <main className="bg-white relative w-full h-full mt-24 rounded-t-3xl overflow-auto">
-        <div className="flex justify-center ">
-          <div className=" text-black md:mt-40 lg:w-1/3 mt-20">
-            <form onSubmit={submitShoppingListItemHandler}>
-              {shoppingListItemNameExists ? (
-                <p>
-                  {`You have ${normalizeString(
-                    shoppingListItemName,
-                  )} in your shopping list already`}
-                </p>
-              ) : null}
-              <div className="">
-                <div className="flex justify-center">
-                  {/* <label className="text-black">Name of Item</label>  */}
+        </section>
+      </header>
+      <main className="bg-white relative w-full h-full mt-24 rounded-t-3xl overflow-auto flex justify-center">
+        <section className=" text-black md:mt-40 lg:w-1/3 mt-20">
+          <form onSubmit={submitShoppingListItemHandler}>
+            {shoppingListItemNameExists ? (
+              <p>
+                {`You have ${normalizeString(
+                  shoppingListItemName,
+                )} in your shopping list already`}
+              </p>
+            ) : null}
+            <section className="flex justify-center">
+              <input
+                type="text"
+                placeholder="Add Item..."
+                value={shoppingListItemName}
+                onChange={shoppingListItemNameHandler}
+                className="border text-gray-900 md:w-2/3 px-2 py-2 md:px-4 md:py-3 mb-8 rounded focus:ring-2 focus:ring-green-600 focus:border-transparent outline-none focus:bg-green-100 text-center"
+              />
+            </section>
+            <p className="text-center md:mb-8 mb-4">
+              How soon are you likely to buy it again?
+            </p>
+            <section className="flex flex-col sm:flex-row sm:space-x-4 text-black md:mb-8 mb-4">
+              <section className="xl:w-1/3 mb-4">
+                <label>
                   <input
-                    type="text"
-                    placeholder="Add Item..."
-                    value={shoppingListItemName}
-                    onChange={shoppingListItemNameHandler}
-                    className="border text-gray-900 md:w-2/3 px-2 py-2 md:px-4 md:py-3 mb-8 rounded focus:ring-2 focus:ring-green-600 focus:border-transparent outline-none focus:bg-green-100"
+                    type="radio"
+                    name="next_purchase"
+                    value="7"
+                    checked={daysLeftForNextPurchase === 7}
+                    onChange={daysLeftForNextPurchaseHandler}
+                    className="mr-4"
                   />
-                </div>
-              </div>
-              <div className="text-center md:mb-8 mb-4">
-                <p className="">How soon are you likely to buy it again?</p>
-              </div>
-              <div className="flex flex-col sm:flex-row sm:space-x-4 text-black md:mb-8 mb-4">
-                <div className="xl:w-1/3 mb-4">
-                  <label>
-                    <input
-                      type="radio"
-                      name="next_purchase"
-                      value="7"
-                      checked={daysLeftForNextPurchase === 7}
-                      onChange={daysLeftForNextPurchaseHandler}
-                      className="mr-4"
-                    />
-                    Soon
-                  </label>
-                </div>
-                <div className="xl:w-1/3 mb-4">
-                  <label>
-                    <input
-                      type="radio"
-                      name="next_purchase"
-                      value="14"
-                      checked={daysLeftForNextPurchase === 14}
-                      onChange={daysLeftForNextPurchaseHandler}
-                      className="mr-4"
-                    />
-                    Kind of soon
-                  </label>
-                </div>
-                <div className="xl:w-1/3 mb-4">
-                  <label>
-                    <input
-                      type="radio"
-                      name="next_purchase"
-                      value="30"
-                      checked={daysLeftForNextPurchase === 30}
-                      onChange={daysLeftForNextPurchaseHandler}
-                      className="mr-4"
-                    />
-                    Not soon
-                  </label>
-                </div>
-              </div>
-              <div className="text-center mt-8">
-                <button
-                  className="border py-2 px-3 hover:shadow-hover justify-center rounded-md bg-white text-black shadow-bottom w-32 md:w-60"
-                  type="submit"
-                >
-                  Add Item
-                </button>
-              </div>
-            </form>
-          </div>
+                  Soon
+                </label>
+              </section>
+              <section className="xl:w-1/3 mb-4">
+                <label>
+                  <input
+                    type="radio"
+                    name="next_purchase"
+                    value="14"
+                    checked={daysLeftForNextPurchase === 14}
+                    onChange={daysLeftForNextPurchaseHandler}
+                    className="mr-4"
+                  />
+                  Kind of soon
+                </label>
+              </section>
+              <section className="xl:w-1/3 mb-4">
+                <label>
+                  <input
+                    type="radio"
+                    name="next_purchase"
+                    value="30"
+                    checked={daysLeftForNextPurchase === 30}
+                    onChange={daysLeftForNextPurchaseHandler}
+                    className="mr-4"
+                  />
+                  Not soon
+                </label>
+              </section>
+            </section>
+            <section className="text-center mt-8">
+              <button
+                className="border py-2 px-3 hover:shadow-hover justify-center rounded-md bg-white text-black shadow-bottom w-32 md:w-60"
+                type="submit"
+              >
+                Add Item
+              </button>
+            </section>
+          </form>
+        </section>
 
-          <Nav />
-          <ItemListButton />
-        </div>
+        <Nav />
+        <ItemListButton />
       </main>
     </div>
   );
