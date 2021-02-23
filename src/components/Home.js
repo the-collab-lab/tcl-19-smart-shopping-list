468af748cb894e6eaa0ba67aee6a13d0ae881e00
YuriDevAT <JUndeutsch@gmx.at>
2021-02-23 20:31:17 +0100

    created basic styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,8 @@ import { useHistory } from 'react-router-dom';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import firebase from '../lib/firebase';
 import getToken from '../lib/tokens';
+import { ReactComponent as CartIcon } from '../img/cart2.svg';
+import paid from '../img/paid.gif';
 
 const db = firebase.firestore().collection('shopping_list');
 
@@ -42,17 +44,18 @@ const Home = () => {
   };
 
   return (
-    <div className="bg-green-500 h-screen w-screen flex flex-col items-center justify-around text-center text-white text-lg font-light">
-      <div className="bg-gray-200 w-full py-16">
-        <h1 className="text-gray-900 text-4xl">
-          Welcome to Smart Shopping App
+    <div className="bg-gradient-to-b from-green-300 to-blue-700 min-h-screen w-screen flex flex-col items-center justify-around text-center text-gray-100 text-lg font-light">
+      <div className=" w-full pt-4 flex flex-col items-center">
+        <h1 className="text-gray-900 font-normal text-4xl leading-loose">
+          Welcome to <br />
+          Smart Shopping App
         </h1>
+        <span className="sm:hidden">
+          <CartIcon />
+        </span>
+        <img className="hidden m-auto w-12" src={paid} alt="symbol" />
       </div>
-      <button
-        type="submit"
-        onClick={newList}
-        className="bg-green-400 py-2 px-4 rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-opacity-50 shadow-md font-light"
-      >
+      <button type="submit" onClick={newList} className="btn">
         Create List
       </button>
       <div>
@@ -60,23 +63,20 @@ const Home = () => {
       </div>
       <div>
         <p>
-          Join an Existing list <br />
+          Join an existing list <br />
           by entering the three word token.
         </p>
       </div>
-      <form onSubmit={submitToken} className="flex flex-col">
+      <form onSubmit={submitToken} className="flex flex-col pb-4">
         <label htmlFor="token">Enter token</label>
         <input
           type="text"
           id="token"
           value={existingToken}
           onChange={tokenHandler}
-          className="my-2 py-2 rounded-md border border-transparent focus:outline-none focus:ring-2 focus:ring-green-600 focus:border-transparent shadow-md text-gray-900"
+          className="my-2 py-2 shadow-hover rounded-md border-transparent focus:bg-green-300 focus:outline-none focus:ring-2 focus:ring-green-700 focus:border-transparent text-gray-900"
         />
-        <button
-          type="submit"
-          className="bg-green-400 py-2 px-4 rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-opacity-50 shadow-md font-light"
-        >
+        <button type="submit" className="btn">
           Join list
         </button>
       </form>
