1f734f33c869254651788c384a7669180550897d
Rebecca Botha <rb50@users.noreply.github.com>
2021-02-26 10:06:56 +0000

    align radio buttons

--- src/components/AddItems.js
+++ src/components/AddItems.js
@@ -136,7 +136,7 @@ const AddItemsToList = () => {
                     value="7"
                     checked={daysLeftForNextPurchase === 7}
                     onChange={daysLeftForNextPurchaseHandler}
-                    className="mr-2 h-5 w-5 mb-1 text-green-600 focus:ring-1 focus:ring-green-600 cursor-pointer"
+                    className="mb-1 mr-2 h-5 w-5 text-green-600 focus:ring-1 focus:ring-green-600 cursor-pointer"
                   />
                   Soon
                 </label>
@@ -149,7 +149,7 @@ const AddItemsToList = () => {
                     value="14"
                     checked={daysLeftForNextPurchase === 14}
                     onChange={daysLeftForNextPurchaseHandler}
-                    className="mr-2 h-5 w-5 mb-4 md:mb-0 md:-mt-1 my-4 md:my-0 text-green-600 focus:ring-1 focus:ring-green-600 cursor-pointer "
+                    className="mt-3 mb-4 md:mb-0 md:-mt-1 mr-2 h-5 w-5 text-green-600 focus:ring-1 focus:ring-green-600 cursor-pointer"
                   />
                   Kind of soon
                 </label>
@@ -162,7 +162,7 @@ const AddItemsToList = () => {
                     value="30"
                     checked={daysLeftForNextPurchase === 30}
                     onChange={daysLeftForNextPurchaseHandler}
-                    className="mr-2 h-5 w-5 -mt-1 text-green-600 focus:ring-1 focus:ring-green-600 cursor-pointer"
+                    className="mr-2 h-5 w-5 text-green-600 focus:ring-1 focus:ring-green-600 cursor-pointer -mt-1"
                   />
                   Not soon
                 </label>
